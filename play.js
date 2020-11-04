680edcace34e4c13d3fe52747634b15248ca0c7d
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-04 18:23:08 +0000

    added stdinput and handler to play.js

--- play.js
+++ play.js
@@ -2,3 +2,18 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', data => handleUserInput(data));
+  return stdin;
+};
+
+const handleUserInput = function (keyPress) {
+  if (keyPress === '\u0003') {
+    process.exit();
+  };
+};
